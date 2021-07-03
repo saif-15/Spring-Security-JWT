20f6964c4a5d1a3aa6ad6c680f7c98fed713615c
shady <saifulhaq.15@outlook.com>
2021-07-03 17:08:45 +0500

    second commit

--- src/main/kotlin/com/stechlabs/jwtExample/jwtExample/service/UploadService.kt
+++ src/main/kotlin/com/stechlabs/jwtExample/jwtExample/service/UploadService.kt
@@ -3,13 +3,16 @@ package com.stechlabs.jwtExample.jwtExample.service
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
+import java.nio.charset.StandardCharsets
+
 
 @Service
 class UploadService {
 
     fun uploadFile(file:MultipartFile):String{
 
-        val content=file.bytes.toString()
+        val content = String(file.bytes)
+        System.out.println(content)
         file.transferTo(File("C:\\Users\\Saif\\Desktop\\jwtExample\\uploads\\${file.originalFilename}"))
         return content
     }
